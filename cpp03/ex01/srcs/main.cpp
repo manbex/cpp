ed667938a47d5c5074f2c6037a90a574178b05d2
Max BENICHOU <mbenicho@paul-f4Br7s4.clusters.42paris.fr>
2023-06-30 09:57:41 +0200

    better tests

--- cpp03/ex01/srcs/main.cpp
+++ cpp03/ex01/srcs/main.cpp
@@ -3,11 +3,19 @@
 int	main()
 {
 	ScavTrap	test("Test");
-	
+
+	std::cout << std::endl;	
 	test.info();
-	test.takeDamage(2);
-	test.beRepaired(2);
+	test.takeDamage(50);
+	test.info();
+	test.beRepaired(35);
+	test.info();
+	std::cout << std::endl;	
+
 	test.attack("Target");
 	test.guardGate();
+	test.info();
+	std::cout << std::endl;	
+
 	return (0);
 }
