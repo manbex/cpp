30e79ff94f7a947fad829a5c954f7c29cc67e4dd
Max BENICHOU <mbenicho@paul-f4Br7s4.clusters.42paris.fr>
2024-03-29 11:27:14 +0100

    test improved

--- cpp07/ex00/srcs/main.cpp
+++ cpp07/ex00/srcs/main.cpp
@@ -9,6 +9,7 @@ int	main()
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << std::endl;
+
 	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
 	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
 	std::cout << std::endl;
@@ -17,11 +18,12 @@ int	main()
 	std::string d = "chaine2";
 
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	::swap(c, d);
+	::swap<std::string>(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << std::endl;
-	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
-	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+
+	std::cout << "min(c, d) = " << ::min<std::string>( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max<std::string>( c, d ) << std::endl;
 	std::cout << std::endl;
 
 	return (0);
